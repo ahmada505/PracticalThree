7d9664dd66e9a9d1d5e75569b25297e80b016fd0
Aness Ahmad <anessahmad20@gmail.com>
2018-10-07 17:30:23 +0200

    added class animation and state

---
+++ FSMPattern/FSMPattern/Animation.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "Animation.h"
+
+
+Animation::Animation()
+{
+	current = new Idle();
+}
+
+void Animation::idle()
+{
+	current->idle(this);
+}
+
+void Animation::jumping()
+{
+	current->jumping(this);
+}
+
+void Animation::climbing()
+{
+	current->climbing(this);
+}
+
+
+
---
+++ FSMPattern/FSMPattern/State.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Animation.h"
+#include <iostream>
+
+using namespace std;
+
+class State
+{
+public:
+	virtual void idle(Animation* a)
+	{
+		cout << "State::Idling" << endl;
+	}
+
+	virtual void jumping(Animation* a)
+	{
+		cout << "State::Jumping" << endl;
+	}
+
+	virtual void climbing(Animation* a)
+	{
+		cout << "State::Climbing" << endl;
+	}
+
+};
+
